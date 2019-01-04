bebb036390828041030eb1a3dd3cc8a6ad3a5a9a
James Davis <jrdavismaine@gmail.com>
2019-01-04 18:20:40 -0500

    Added build dir

---
+++ build/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _service = require('./service');
+
+var _service2 = _interopRequireDefault(_service);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _service2.default;
