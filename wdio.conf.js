38cbc3763219edf4fca5bae72222809e66dfdb7c
James Davis <jrdavismaine@gmail.com>
2019-01-07 23:37:23 -0500

    Renamed test folder to e2e

--- wdio.conf.js
+++ wdio.conf.js
@@ -21,7 +21,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/**/*.spec.js'
+        './e2e/**/*.spec.js'
     ],
     // Patterns to exclude.
     exclude: [
