f28b4db846b27c5949742e55ee39b3a84066313b
James Davis <jrdavismaine@gmail.com>
2019-01-30 19:30:45 -0500

    Removed negative test

--- e2e/element/clearElement.spec.js
+++ e2e/element/clearElement.spec.js
@@ -20,23 +20,4 @@ describe('Clear element test', () => {
         loginForm.username.clearElement();
         assert.equal(loginForm.username.getValue(), '');
     });
-
-    it('Enter data, hide text-field, confirm \'element not interactable\' error is thrown', () => {
-        const script = `
-            var e = document.createElement('input');
-            e.type = 'text'; 
-            e.style.visibility = 'visible';
-            e.id = 'elem';
-            document.forms[0].appendChild(e);`;
-        browser.execute(script);
-        browser.pause(2000);
-        const input = browser.$('#elem');
-        input.setValue(email);
-        browser.execute('document.getElementById(\'elem\').style.visibility = \'hidden\';');
-        try {
-            input.clearElement();
-        } catch (error) {
-            assert(error.message.indexOf('element not interactable') > -1);
-        }
-    });
 });
