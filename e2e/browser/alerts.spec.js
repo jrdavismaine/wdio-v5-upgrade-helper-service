91464b88e8b169d6009091e7f5c4e257a40eca84
James Davis <jrdavismaine@gmail.com>
2019-02-02 06:54:32 -0500

    Added additional browser e2e tests

---
+++ e2e/browser/alerts.spec.js
@@ -0,0 +1,60 @@
+import assert from 'assert';
+
+describe('Alert commands test suite: ', () => {
+    before(() => {
+        browser.url('/javascript_alerts');
+        browser.pause(3000);
+    });
+
+    context('Alert box: alertAccept, alertText command alias test', () => {
+        before(() => {
+            browser.$$('button')[0].click();
+        });
+
+        it('alertText', () => {
+            assert.equal(browser.alertText(), 'I am a JS Alert');
+        });
+
+        it('alertAccept', () => {
+            browser.alertAccept();
+            assert.equal($('#result').getText(), 'You successfuly clicked an alert');
+        });
+    });
+
+    context('Alert box: alertDismiss command alias test', () => {
+        before(() => {
+            browser.$$('button')[0].click();
+        });
+
+        it('Accept alert', () => {
+            browser.alertDismiss();
+            assert.equal($('#result').getText(), 'You successfuly clicked an alert');
+        });
+    });
+
+    context('Confirm box: alertAccept, alertText command alias test', () => {
+        before(() => {
+            browser.$$('button')[1].click();
+        });
+
+        it('alertText', () => {
+            assert.equal(browser.alertText(), 'I am a JS Confirm');
+        });
+
+        it('alertAccept', () => {
+            browser.alertAccept();
+            assert.equal($('#result').getText(), 'You clicked: Ok');
+        });
+    });
+
+    context('Confirm box: alertDismiss command alias test', () => {
+        before(() => {
+            browser.$$('button')[1].click();
+        });
+
+        it('alertDismiss', () => {
+            browser.alertDismiss();
+            assert.equal($('#result').getText(), 'You clicked: Cancel');
+        });
+    });
+});
